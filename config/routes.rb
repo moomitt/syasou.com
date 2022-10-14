33fcf63c4ad2154deb7b59808c4b87a4ad060c1d
moomitt <km14.mcucthsn@gmail.com>
2022-10-14 02:34:08 +0000

    [0] Admin controller/routing作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,27 @@
 Rails.application.routes.draw do
+
   devise_for :admins, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
+
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
-  
+
+  namespace :admin do
+    root to: 'homes#top'
+    resources :users, only: [:index, :show, :edit, :update]
+    get 'users/:id/post' => 'users#post', as: 'user_post'
+    resources :posts, only: [:index, :show, :destroy] do
+      member do
+        delete :post_comment_destroy
+      end
+    end
+    get 'posts/search'
+    resources :tags, only: [:index, :destroy]
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
