a695a8ec1dff8c8219e1ee7eefa6dadd6ec3cd84
moomitt <km14.mcucthsn@gmail.com>
2022-10-17 05:40:16 +0000

    [20] 新規投稿機能（路線図から選択）③

--- app/javascript/packs/new.js
+++ app/javascript/packs/new.js
@@ -17,13 +17,11 @@ function init() {                         // Rosen：イニシャライザ（ア
         document.getElementById('startStationCord').value=stations[0].code;
         document.getElementById('endStationCord').value=stations[1].code;
       });
-      Rosen.getLineCodeBySectionCode([section.code])
-      .then(function(line_code) {
-        rosen.getLineByCode(line_code)
-        .then(function(line) {
-          $('#LineName').text(line.name);
-          document.getElementById('LineCord').value=line.code;
-        });
+      const line_code = Rosen.getLineCodeBySectionCode([section.code]);
+      rosen.getLineByCode(line_code)
+      .then(function(line) {
+        $('#LineName').text(line.name);
+        document.getElementById('LineCord').value=line.code;
       });
     });
   });
