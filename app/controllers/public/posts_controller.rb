d4a4d40314ff8113b12efd857ac2afc902770938
moomitt <km14.mcucthsn@gmail.com>
2022-10-19 05:28:58 +0000

    [22] 投稿編集・削除機能

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -25,6 +25,21 @@ class Public::PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
   def index
     @all_posts = Post.all
     @map_posts = Array::new
