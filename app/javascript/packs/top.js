b6145a29f1498bb2b1435b2490cc41cba9ea2c9f
moomitt <km14.mcucthsn@gmail.com>
2022-10-24 08:58:22 +0000

    [29] トップページ　メインイメージ（スライドショー）

---
+++ app/javascript/packs/top.js
@@ -0,0 +1,12 @@
+$(function(){
+  var mySwiper = new Swiper ('.swiper-container', {
+    speed: 500,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+		slidesPerView: 1,      //画像を何枚表示するか
+		spaceBetween: 30,      //何ピクセル画像の間隔をあけるか
+		loop: true,            //最後の画像までいったらループする
+	});
+});
