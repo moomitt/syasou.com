e69a4a54c64d98268f83de72b726b23d3e9c80fa
moomitt <km14.mcucthsn@gmail.com>
2022-10-26 23:15:26 +0000

    [58] レイアウト　トップページ上部・修正

--- app/javascript/packs/top.js
+++ app/javascript/packs/top.js
@@ -1,10 +1,11 @@
 $(function(){
   var mySwiper = new Swiper ('.swiper-container', {
-    speed: 500,
+    effect: 'fade',
     autoplay: {
-      delay: 4000,
+      delay: 3000,
       disableOnInteraction: false,
     },
+    speed: 2000,
 		slidesPerView: 1,      //画像を何枚表示するか
 		spaceBetween: 30,      //何ピクセル画像の間隔をあけるか
 		loop: true,            //最後の画像までいったらループする
