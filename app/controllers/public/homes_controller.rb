d8bfad236aa4fc83d585e4148ef4d2001eebbcd6
moomitt <km14.mcucthsn@gmail.com>
2022-10-27 22:57:55 +0000

    [29] トップページ　新着投稿

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,4 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
+    @pupular_posts = Post.left_joins(:bookmarks).group(:id).order('count(bookmarks.post_id) desc').limit(3)
+    @new_posts = Post.order('id desc').limit(6)
   end
 end
--- app/javascript/packs/top.js
+++ app/javascript/packs/top.js
@@ -1,13 +1,32 @@
 $(function(){
-  var mySwiper = new Swiper ('.swiper-container', {
+  var mySwiper1 = new Swiper ('.slider1', {
     effect: 'fade',
     autoplay: {
       delay: 3000,
       disableOnInteraction: false,
     },
     speed: 2000,
-		slidesPerView: 1,      //画像を何枚表示するか
-		spaceBetween: 30,      //何ピクセル画像の間隔をあけるか
-		loop: true,            //最後の画像までいったらループする
+		slidesPerView: 1,       //画像を何枚表示するか
+		spaceBetween: 30,       //何ピクセル画像の間隔をあけるか
+		loop: true,             //最後の画像までいったらループする
+	});
+
+	var mySwiper2 = new Swiper ('.slider2', {
+	  effect: 'slide',
+	  autoWidth: false,
+		slidesPerView: 3,       //画像を何枚表示するか
+		slidesPerGroup: 3,
+		spaceBetween: 10,       //何ピクセル画像の間隔をあけるか
+		centeredSlides : false,  //見切らせたい場合メイン画像をセンターにもってくるか,
+		loop: true,             //最後の画像までいったらループする
+		pagination: {
+		 el: '.swiper-pagination',
+		 type: 'bullets',
+		 clickable: true,
+		},
+		navigation: {
+		 nextEl: '.swiper-button-next',
+		 prevEl: '.swiper-button-prev',
+		}
 	});
 });
