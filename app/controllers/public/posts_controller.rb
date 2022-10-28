2adfad0ed88fa53b5b93e2ebc80dd161e54c3771
moomitt <km14.mcucthsn@gmail.com>
2022-10-28 07:47:55 +0000

    [46] 追加機能　タブ表示（エリア・都道府県検索画面）

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -78,65 +78,83 @@ class Public::PostsController < ApplicationController
   def search_area
     if params[:id] == "1"
       @area_name = "北海道"
-      @all_posts = Post.where(start_station_prefecture: 1)
+      all_posts = Post.where(start_station_prefecture: 1)
       .or(Post.where(end_station_prefecture: 1))
+      @popular_posts = all_posts.sort{|a,b| b.bookmarks.size <=> a.bookmarks.size}
+      @new_posts = all_posts.order('id desc')
     end
     if params[:id] == "2"
       @area_name = "東北"
       ids = [2, 3, 4, 5, 6, 7]
       @prefectures = Prefecture.find(ids)
-      @all_posts = Post.where(start_station_prefecture: 2..7)
+      all_posts = Post.where(start_station_prefecture: 2..7)
       .or(Post.where(end_station_prefecture: 2..7))
+      @popular_posts = all_posts.sort{|a,b| b.bookmarks.size <=> a.bookmarks.size}
+      @new_posts = all_posts.order('id desc')
     end
     if params[:id] == "3"
       @area_name = "関東"
       ids = [8, 9, 10, 11, 12, 13, 14]
       @prefectures = Prefecture.find(ids)
-      @all_posts = Post.where(start_station_prefecture: 8..14)
+      all_posts = Post.where(start_station_prefecture: 8..14)
       .or(Post.where(end_station_prefecture: 8..14))
+      @popular_posts = all_posts.sort{|a,b| b.bookmarks.size <=> a.bookmarks.size}
+      @new_posts = all_posts.order('id desc')
     end
     if params[:id] == "4"
       @area_name = "中央・北陸・東海"
       ids = [15, 16, 17, 18, 19, 20, 21, 22, 23]
       @prefectures = Prefecture.find(ids)
-      @all_posts = Post.where(start_station_prefecture: 15..23)
+      all_posts = Post.where(start_station_prefecture: 15..23)
       .or(Post.where(end_station_prefecture: 15..23))
+      @popular_posts = all_posts.sort{|a,b| b.bookmarks.size <=> a.bookmarks.size}
+      @new_posts = all_posts.order('id desc')
     end
     if params[:id] == "5"
       @area_name = "近畿"
       ids = [24, 25, 26, 27, 28, 29, 30]
       @prefectures = Prefecture.find(ids)
-      @all_posts = Post.where(start_station_prefecture: 24..30)
+      all_posts = Post.where(start_station_prefecture: 24..30)
       .or(Post.where(end_station_prefecture: 24..30))
+      @popular_posts = all_posts.sort{|a,b| b.bookmarks.size <=> a.bookmarks.size}
+      @new_posts = all_posts.order('id desc')
     end
     if params[:id] == "6"
       @area_name = "中国"
       ids = [31, 32, 33, 34, 35]
       @prefectures = Prefecture.find(ids)
-      @all_posts = Post.where(start_station_prefecture: 31..35)
+      all_posts = Post.where(start_station_prefecture: 31..35)
       .or(Post.where(end_station_prefecture: 31..35))
+      @popular_posts = all_posts.sort{|a,b| b.bookmarks.size <=> a.bookmarks.size}
+      @new_posts = all_posts.order('id desc')
     end
     if params[:id] == "7"
       @area_name = "四国"
       ids = [36, 37, 38, 39]
       @prefectures = Prefecture.find(ids)
-      @all_posts = Post.where(start_station_prefecture: 36..39)
+      all_posts = Post.where(start_station_prefecture: 36..39)
       .or(Post.where(end_station_prefecture: 36..39))
+      @popular_posts = all_posts.sort{|a,b| b.bookmarks.size <=> a.bookmarks.size}
+      @new_posts = all_posts.order('id desc')
     end
     if params[:id] == "8"
       @area_name = "九州・沖縄"
       ids = [40, 41, 42, 43, 44, 45, 46, 47]
       @prefectures = Prefecture.find(ids)
-      @all_posts = Post.where(start_station_prefecture: 40..47)
+      all_posts = Post.where(start_station_prefecture: 40..47)
       .or(Post.where(end_station_prefecture: 40..47))
+      @popular_posts = all_posts.sort{|a,b| b.bookmarks.size <=> a.bookmarks.size}
+      @new_posts = all_posts.order('id desc')
     end
   end
 
   def search_prefecture
     if params[:id]
       @prefecture = Prefecture.find(params[:id])
-      @all_posts = Post.where(start_station_prefecture: params[:id])
+      all_posts = Post.where(start_station_prefecture: params[:id])
       .or(Post.where(end_station_prefecture: params[:id]))
+      @popular_posts = all_posts.sort{|a,b| b.bookmarks.size <=> a.bookmarks.size}
+      @new_posts = all_posts.order('id desc')
       id = params[:id].to_i
       if id < 8
         @area_id = 2
