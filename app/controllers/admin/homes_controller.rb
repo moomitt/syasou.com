61c3e3bd8153af49d423779f971fc4d50e321ee8
moomitt <km14.mcucthsn@gmail.com>
2022-10-29 07:40:54 +0000

    [35,57-64] 管理者トップページ+管理者画面レイアウト

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,6 @@
 class Admin::HomesController < ApplicationController
   def top
+    @popular_posts = Post.find(Bookmark.group(:post_id).order('count(id) desc').limit(6).pluck(:post_id))
+    @new_posts = Post.order('id desc').limit(6)
   end
 end
