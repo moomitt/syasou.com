6bf289c8ad9486a08e6ee46e1aca2c8d2e17c19d
moomitt <km14.mcucthsn@gmail.com>
2022-10-29 01:00:38 +0000

    [28-30] 管理者　投稿詳細画面＋投稿・コメント削除機能

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,2 +1,8 @@
 class Admin::CommentsController < ApplicationController
+  def destroy
+    post = Post.find(params[:post_id])
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to admin_post_path(post.id)
+  end
 end
--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -3,5 +3,20 @@ class Admin::PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
+    @user = @post.user
+    @comments = Comment.where(post_id: @post.id)
+    session[:previous_url] = request.referer     #前ページセッションを保存
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.post_images.attached?
+      @post.post_images.purge_later && @post.destroy
+      redirect_to session[:previous_url]
+    else
+      @post.destroy
+      redirect_to session[:previous_url]    #2つ前のページにリダイレクト
+    end
   end
 end
