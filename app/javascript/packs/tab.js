2270a5c4e96f69bc8684207c964862dc1baa8c15
moomitt <km14.mcucthsn@gmail.com>
2022-11-06 01:19:35 +0000

    [68] tab.js jsコード見直し・コメント追記

--- app/javascript/packs/tab.js
+++ app/javascript/packs/tab.js
@@ -1,24 +1,33 @@
-$('#tab-contents .tab[id != "tab1"]').hide();
+/*global $*/
 
-$('#tab-menu a').on('click', function(event){
-  $('#tab-contents .tab').hide();
-  $('#tab-menu .active').removeClass("active");
-  $(this).addClass("active");
-  $($(this).attr('href')).show();
-  event.preventDefault();
-})
+//タブ表示機能
+$(function(){
+  //初期表示
+  $('#tab-contents .tab[id != "tab1"]').hide();
+  //クリック時のイベント
+  $('#tab-menu a').on('click', function(event){
+    $('#tab-contents .tab').hide();
+    $('#tab-menu .active').removeClass("active");
+    $(this).addClass("active");
+    $($(this).attr('href')).show();
+    event.preventDefault();
+  });
+});
 
-$(".modal-open").on('click',function(){
-  $("#modal-overlay").fadeIn("fast");
-});
-// 閉じるボタンかオーバーレイ部をクリックでモーダルウィンドウ削除
-$("#modal-close,#modal-overlay").on('click',function(){
-  $("#modal-overlay").fadeOut("fast");
-});
-// ログイン・新規登録ボタンを押すと非表示のlink_toをクリックする
-$("#modal-login").on('click',function(){
-  document.getElementById("logInBtn").click();
-});
-$("#modal-signup").on('click',function(){
-  document.getElementById("signUpBtn").click();
+//モーダルウィンドウ（ゲストログイン用）
+$(function(){
+  //表示・非表示
+  $(".modal-open").on('click',function(){
+    $("#modal-overlay").fadeIn("fast");
+  });
+  $("#modal-close,#modal-overlay").on('click',function(){
+    $("#modal-overlay").fadeOut("fast");
+  });
+  // ログイン・新規登録ボタンを押すと非表示のlink_toをクリックする
+  $("#modal-login").on('click',function(){
+    document.getElementById("logInBtn").click();
+  });
+  $("#modal-signup").on('click',function(){
+    document.getElementById("signUpBtn").click();
+  });
 });
